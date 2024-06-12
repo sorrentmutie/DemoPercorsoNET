1744b90214700d935dd0fcb889a7fa26979484e4
Michele Tancorre <micheletancorre@gmail.com>
2024-06-12 10:38:10 +0200

    Multi Thread per analisi testi multipli

--- Cotrap.Core/AnalisiTesto/Program.cs
+++ Cotrap.Core/AnalisiTesto/Program.cs
@@ -1,18 +1,46 @@
 ﻿using Cotrap.Core;
 using Cotrap.Core.AnalisiTesto;
 
-//string fileDaAnalizzare = @"C:\SVILUPPI\CORSO\TestoDaAnlizzare\CostituzioneItaliana.txt";
-string fileDaAnalizzare = @"C:\SVILUPPI\CORSO\TestoDaAnlizzare\5Maggio.txt";
-//string fileDaAnalizzare = @"C:\SVILUPPI\CORSO\TestoDaAnlizzare\us_constitution.json";
+string fileCostituzione = @"C:\SVILUPPI\CORSO\TestoDaAnlizzare\CostituzioneItaliana.txt";
+string fileMaggio = @"C:\SVILUPPI\CORSO\TestoDaAnlizzare\5Maggio.txt";
+string fileUS = @"C:\SVILUPPI\CORSO\TestoDaAnlizzare\us_constitution.json";
 
-Analisi analisi = new Analisi(fileDaAnalizzare);
-var DizionarioParole = analisi.TrovaParolePiuFrequenti(10);
-var parolaLunga = analisi.ParolaPiuLunga();
+//Analisi analisi = new Analisi(fileDaAnalizzare);
+//var DizionarioParole = analisi.TrovaParolePiuFrequenti(10);
+//var parolaLunga = analisi.ParolaPiuLunga();
 //foreach(var entry in DizionarioParole)
 //{
 //    Console.WriteLine($"{entry.Key} {entry.Value}");
     
 //}
 
-Console.WriteLine(parolaLunga);
+
+
+Console.WriteLine($"Current Thread: {Thread.CurrentThread.ManagedThreadId}");
+Console.WriteLine($"Main Program: {Thread.CurrentThread.IsBackground}");
+
+var pathFiles = new string[] { fileCostituzione, fileMaggio, fileUS };
+for (int i = 0; i < 3; i++)
+{
+    ThreadPool.QueueUserWorkItem(new WaitCallback(EsaminaTesto), pathFiles[i]);
+}
+
+
+Console.WriteLine("Fine dell'esecuzione");
+Console.ReadLine();
+
+
+void EsaminaTesto(object? path)
+{
+    var id = Thread.CurrentThread.ManagedThreadId;
+    Console.WriteLine($"Path = {path}");   
+    Console.WriteLine($"Ciao sto leggendo {id} {Thread.CurrentThread.IsBackground}");
+    if (path is not null)
+    {
+        Analisi analisi = new Analisi((string)path);
+        var parolaLunga = analisi.ParolaPiuLunga();
+        Console.WriteLine($"Parola più lunga: {parolaLunga} del thread: {id}");
+        Console.WriteLine($"Ho finito di fare qualcosa  {id}");
+    }
+}
 
