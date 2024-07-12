90a2c36a6bc6ca02edcf9999acbda337cc90c73c
salvatore sorrentino <salvatore.sorrentino@live.com>
2024-07-12 12:57:19 +0200

    Bug fix sui prodotti

--- Cotrap.Core/Cotrap.API/Services/ServizioDatiCliente.cs
+++ Cotrap.Core/Cotrap.API/Services/ServizioDatiCliente.cs
@@ -18,11 +18,30 @@ public class ServizioDatiCliente : ICustomerData
                             .ThenInclude(od => od.Product)                            
                             .FirstOrDefaultAsync(x => x.Id == Id);
 
-        var products = await context.OrderDetails.Include(od => od.Product)
+        var orders = await context.Orders
+            .Include(o => o.OrderDetails)
+            .ThenInclude(od => od.Product)
             .ThenInclude(p => p.Supplier)
-            .Select(x => new ProdottoDTO { Id = x.ProductId, Nome = x.Product.ProductName, Fornitore = x.Product.Supplier.CompanyName })
-            .Distinct()
-            .ToListAsync();
+            .Where(o => o.CustomerId == Id).ToListAsync();
+
+        var products = new List<ProdottoDTO>();
+        foreach (var o in orders) {
+            foreach (var od in o.OrderDetails) { 
+                  products.Add(new ProdottoDTO
+                  {
+                       Id = od.ProductId, Nome = od.Product.ProductName, 
+                      Fornitore = od.Product.Supplier.CompanyName
+                  });
+            }
+        }
+
+
+
+        //var products = await context.OrderDetails.Include(od => od.Product)
+        //    .ThenInclude(p => p.Supplier)
+        //    .Select(x => new ProdottoDTO { Id = x.ProductId, Nome = x.Product.ProductName, Fornitore = x.Product.Supplier.CompanyName })
+        //    .Distinct()
+        //    .ToListAsync();
 
         if (customer is not null)
         {
